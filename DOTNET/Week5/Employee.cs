b017447dd7b7e1c0cdf0fe1be20ea8affe8db621
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2026-01-31 16:59:36 +0530

    Add files via upload

---
+++ DOTNET/Week5/Employee.cs
@@ -0,0 +1,19 @@
+﻿using SalaryCalculator;
+namespace EmployeeApp
+{
+    internal class Employee
+    {
+        static void Main(string[] args)
+        {
+            //Exercise3
+            Console.Write("Enter Employee Name:");
+            string name = Console.ReadLine();
+            Console.Write("Enter Basic Salary:");
+            double basicSalary = double.Parse(Console.ReadLine());
+            double netSalary = Salary.CalculateNetSalary(basicSalary);
+            Console.WriteLine("Employee Name:" + name);
+            Console.WriteLine("Basic Salary:" + basicSalary);
+            Console.WriteLine("Net Salary:" + netSalary);
+        }
+    }
+}
---
+++ DOTNET/Week5/Salary.cs
@@ -0,0 +1,31 @@
+﻿namespace SalaryCalculator
+{
+    public class Salary
+    {
+        public static double CalculateNetSalary(double basicSalary)
+        {
+            try
+            {
+                if (basicSalary <= 0)
+                {
+                    throw new Exception("Basic salary must be greater than 0");
+                }
+                double hra = 0.20 * basicSalary;
+                double da = 0.10 * basicSalary;
+                double pf = 0;
+
+                if (basicSalary >= 15000)
+                {
+                    pf = 0.12 * basicSalary;
+                }
+                double netSalary = basicSalary + hra + da - pf;
+                return netSalary;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return 0;
+            }
+        }
+    }
+}
