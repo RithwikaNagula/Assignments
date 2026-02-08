21596d35312b480e839a9c5c1cb139ebd9ab9c19
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2026-02-08 21:47:38 +0530

    Add files via upload

---
+++ DOTNET/Week6/Requirement2/Program.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Requirement2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // get parking lot name
+            Console.WriteLine("Enter the name of the Parking Lot:");
+            string name = Console.ReadLine();
+
+            // create parking lot
+            ParkingLot parkingLot = new ParkingLot(name, new List<Vehicle>());
+
+            
+            while (true)
+            {
+                Console.WriteLine("1.Add Vehicle");
+                Console.WriteLine("2.Delete Vehicle");
+                Console.WriteLine("3.Display Vehicles");
+                Console.WriteLine("4.Exit");
+
+                Console.WriteLine("Enter your choice:");
+
+                try
+                {
+                    int choice = int.Parse(Console.ReadLine());
+
+                    if (choice == 1)
+                    {
+                        try
+                        {
+                            string input = Console.ReadLine();
+
+                            Vehicle v = Vehicle.CreateVehicle(input);
+
+                            parkingLot.AddVehicleToParkingLot(v);
+
+                            Console.WriteLine("Vehicle successfully added");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("invalid vehicle details");
+                        }
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.WriteLine("Enter the registration number of the vehicle to be deleted:");
+                        string reg = Console.ReadLine();
+
+                        bool result =
+                            parkingLot.RemoveVehicleFromParkingLot(reg);
+
+                        if (result)
+                            Console.WriteLine("Vehicle successfully deleted");
+                        else
+                            Console.WriteLine("Vehicle not found in parkinglot");
+                    }
+                    else if (choice == 3)
+                    {
+                        parkingLot.DisplayVehicles();
+                    }
+                    else if (choice == 4)
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    // handle invalid menu input
+                    Console.WriteLine("invalid choice");
+                }
+            }
+        }
+    }
+}
---
+++ DOTNET/Week6/Requirement2/Vehicle.cs
@@ -0,0 +1,68 @@
+﻿namespace Requirement2
+{
+    internal class Vehicle
+    {
+        private string _registrationNo;
+        private string _name;
+        private string _type;
+        private double _weight;
+        private Ticket _ticket;
+
+        public string RegistrationNo { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Weight { get; set; }
+        public Ticket Ticket { get; set; }
+
+        // default constructor
+        public Vehicle() { }
+
+        // parameterized constructor
+        public Vehicle(string _registrationNo, string _name, string _type, double _weight, Ticket _ticket)
+        {
+            this.RegistrationNo = _registrationNo;
+            this.Name = _name;
+            this.Type = _type;
+            this.Weight = _weight;
+            this.Ticket = _ticket;
+        }
+
+        // static method to create vehicle object from string input
+        public static Vehicle CreateVehicle(string detail)
+        {
+            try
+            {
+                // check empty input
+                if (string.IsNullOrWhiteSpace(detail))
+                     throw new Exception("empty input");
+
+                string[] data = detail.Split(',');
+
+                // must contain 7 fields
+                if (data.Length != 7)
+                    throw new Exception("wrong format");
+
+                string regNo = data[0];
+                string name = data[1];
+                string type = data[2];
+                double weight = double.Parse(data[3]);
+
+                string ticketNo = data[4];
+
+                DateTime parkedTime =
+                    DateTime.ParseExact(data[5],"dd-MM-yyyy HH:mm:ss", null);
+
+                double cost = double.Parse(data[6]);
+
+                Ticket t = new Ticket(ticketNo, parkedTime, cost);
+
+                return new Vehicle(regNo, name, type, weight, t);
+            }
+            catch
+            {
+                // throw again so main handles it
+                throw new Exception("invalid vehicle details");
+            }
+        }
+    }
+}
