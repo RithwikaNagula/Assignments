0d4de043c5d735c954ca97e4c2b311122c73ee40
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2026-02-09 20:07:33 +0530

    Add files via upload

---
+++ DOTNET/Day29/Program.cs
@@ -0,0 +1,45 @@
+﻿using Day29;
+using System;
+namespace Day29
+{
+    delegate void Print();
+    class Money
+        {
+            protected uint note;
+            protected uint coin;
+            public Money(uint n, uint c)
+            {
+                this.note = n;
+                this.coin = c;
+            }
+        }
+    class Rupee : Money
+        {
+            public Rupee(uint rupees, uint paise) : base(rupees, paise) { }
+            public void Display()
+            {
+                Console.WriteLine("Rs. {0}.{1}", note, coin);
+            }
+        }
+    
+    class Dollar : Money
+        {
+            public Dollar(uint dollar, uint cent) : base(dollar, cent) { }
+            public void Info()
+            {
+                Console.WriteLine("${0}.{1}", note, coin);
+            }
+        }
+    class Program
+        {
+            static void Main()
+            {
+                Rupee m1 = new Rupee(1000, 55);
+                Dollar m2 = new Dollar(100, 75);
+                Print GP = m1.Display;
+                GP();
+                GP = m2.Info;
+                GP();
+            }
+        }
+}
