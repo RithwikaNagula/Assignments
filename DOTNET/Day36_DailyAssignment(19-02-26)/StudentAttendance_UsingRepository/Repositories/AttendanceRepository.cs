d8d4f489ca869fda954e84b4ec25be9e1dc919ec
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2026-02-20 00:07:11 +0530

    Add files via upload

---
+++ DOTNET/Day36_DailyAssignment(19-02-26)/StudentAttendance_UsingRepository/Repositories/AttendanceRepository.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using StudentAttendance.Data;
+using StudentAttendance.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAttendance.Repositories
+{
+    public class AttendanceRepository : IAttendanceRepository
+    {
+        private readonly AttendanceDbContext _context;
+
+        public AttendanceRepository(AttendanceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Attendance>> GetAllAsync()
+        {
+            return await _context.Attendances.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Attendance>> GetAllWithStudentAsync()
+        {
+            return await _context.Attendances.Include(a => a.Student).ToListAsync();
+        }
+
+        public async Task<Attendance> GetByIdAsync(int id)
+        {
+            return await _context.Attendances.FindAsync(id);
+        }
+
+        public async Task<Attendance> GetByIdWithStudentAsync(int id)
+        {
+            return await _context.Attendances.Include(a => a.Student).FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task AddAsync(Attendance entity)
+        {
+            await _context.Attendances.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Attendance entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var attendance = await _context.Attendances.FindAsync(id);
+            if (attendance != null)
+            {
+                _context.Attendances.Remove(attendance);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
