dc1eef6baf56210b605ef722b7a9fbcd4f94cb3b
RithwikaNagula <155565997+RithwikaNagula@users.noreply.github.com>
2026-02-20 00:09:49 +0530

    Add files via upload

---
+++ DOTNET/Day36_DailyAssignment(19-02-26)/JWT_Roles/Program.cs
@@ -0,0 +1,46 @@
+
+using System.Text;
+using JWTRoles.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTRoles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            var app = builder.Build();
+            // JWT Auth properties
+            var jwtKey = builder.Configuration["Jwt:Key"]!;
+            var jwtIssuer = builder.Configuration["Jwt:Issuer"]!;
+            var jwtAudience = builder.Configuration["Jwt:Audience"]!;
+            var keyBytes = Encoding.UTF8.GetBytes(jwtKey);
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
